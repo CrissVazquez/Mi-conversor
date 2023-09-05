9683a836839779b354c6c31e71fd92265c6ea46c
CrissVazquez <cristianlvr4@gmail.com>
2023-09-05 01:29:09 -0600

    Actualice el tipo de cambio al dia 5 de septiembre de 2023

--- src/Enums/Moneda.java
+++ src/Enums/Moneda.java
@@ -4,15 +4,15 @@ import java.math.BigDecimal;
 
 public enum Moneda {
 	
-	//Cotizacion hecha el 24/08/23
-    DOLAR(BigDecimal.valueOf(16.83)),
-    EURO(BigDecimal.valueOf(18.15)),
-    LIBRA_ESTERLINA(BigDecimal.valueOf(21.15)),
-    PESO_ARGENTINO(BigDecimal.valueOf(0.048)),
+	//Cotizacion hecha el 05/09/23
+    DOLAR(BigDecimal.valueOf(17.26)),
+    EURO(BigDecimal.valueOf(18.59)),
+    LIBRA_ESTERLINA(BigDecimal.valueOf(21.72)),
+    PESO_ARGENTINO(BigDecimal.valueOf(0.049)),
     PESO_CHILENO(BigDecimal.valueOf(0.0020)),
-	FRANCO_SUIZO(BigDecimal.valueOf(19.29)),
+	FRANCO_SUIZO(BigDecimal.valueOf(19.48)),
 	YEN_JAPONES(BigDecimal.valueOf(0.12)),
-	YUAN_CHINO(BigDecimal.valueOf(2.35));
+	YUAN_CHINO(BigDecimal.valueOf(2.37));
 	
     private BigDecimal FACTOR_CONVERSION;
 
